8445085f298b8524050e1f274d9168c60c3ba015
felix <felixvjr0@gmail.com>
2024-11-15 00:47:21 -0500

    basic cplusplus code to get started

--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <string>;
+
+using std::endl; // defined globally;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello Felix!" << std::endl;
+
+    using namespace std; // so you wouldn't need to use std << cout everytime - using namespace can only be used in effect where it is defined. You can use it gloabally but you should rarely do so.
+
+    // there is a solution to better us 'using namespace' by using what you need.
+
+    using std::cout; // defined within the main function space.
+
+    int test {99}; // unified initialization;
+
+    // type inference by using auto tag
+
+    auto number {4}; // will convert to int at compile time
+
+    string first = "Hello";
+    string last = "World";
+
+    cout << first+" "+last << endl;
+
+    int a = 100;
+
+    int b = 99;
+
+    if(a<=>b > 0)  { // the <=> > 0 is the same as if using a > b. It's called the spaceship operator. It is in comparison to strcmp in C.
+        std::cout << a << " is greater than " << b << std::endl;
+    };
+
+    //logical operators
+    if (b > 0 && b < a) { // this could've been done with two if checks but if both are bool expression depending on what you need, you can use logical operators.
+        std::cout << "True" << std::endl;
+    }
+
+
+
+    std::cout << test << std::endl; // this turns into
+    cout << "Hello without std" << endl; // cout instead of std;
     return 0;
 }
