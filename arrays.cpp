761c093e16666afe7ab4c970f6d3557bc1b3ab00
felix <felixvjr0@gmail.com>
2024-12-29 01:54:13 -0500

    Testin from nvim
    testing from my nvim config

--- arrays.cpp
+++ arrays.cpp
@@ -1,18 +1,19 @@
 //
 // Created by Felix Vargas Jr on 11/19/24.
 //
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 // ranged based for loop
-
+using namespace std;
 
 std::vector<int> numbers(10);
 
 int main() {
-    for(int i : numbers){
-        std::cout << i << std::endl;
-    }
 
-    return 0;
+  for (int i : numbers) {
+    std::cout << i << std::endl;
+  }
+
+  return 0;
 }
